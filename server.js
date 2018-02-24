7918bf037af9f82023f14c56da00ce58fec2838d
Jakub Kosior <jakubjkosior@gmail.com>
2018-02-24 20:31:57 +0100

    server setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const config = require("./config");
+
+const port = config.port || 3000;
+
+const server = app.listen(port, () =>{
+    console.log(`Server is listening on port: ${port}`);
+});
